220920d24b484938b5a21249c091596468d2eb65
Joel Borrero <joel@merlix.com>
2024-01-08 13:36:23 -0500

    Fix in-game sizes and colors

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -10,11 +10,16 @@ ButtonStyle _buttonStyle(Color color, double opacity) => ButtonStyle(
     );
 
 ThemeData darkTheme = ThemeData.dark().copyWith(
+  shadowColor: Colors.white.withOpacity(.5),
+  elevatedButtonTheme:
+      ElevatedButtonThemeData(style: _buttonStyle(AppColors.orange, .1)),
   iconButtonTheme: IconButtonThemeData(style: _buttonStyle(Colors.white, .2)),
   textButtonTheme: TextButtonThemeData(style: _buttonStyle(Colors.white, .2)),
 );
 
 ThemeData lightTheme = ThemeData(
+  elevatedButtonTheme:
+      ElevatedButtonThemeData(style: _buttonStyle(AppColors.orange, .1)),
   iconButtonTheme: IconButtonThemeData(style: _buttonStyle(Colors.black, .1)),
   textButtonTheme: TextButtonThemeData(style: _buttonStyle(Colors.black, .1)),
 );
